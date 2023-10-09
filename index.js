9d978e4e70cfe2303c402eb76267538421d8a550
Adekunle Adesokan <kunle.adesokan9@gmail.com>
2023-10-09 02:42:33 +0200

    insert movies and series data into database, link React frontend to backend

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
+import genres from "./genres.js";
 
-mongoose.connect("mongodb://localhost:27017/streamlet", {
+mongoose.connect("mongodb://localhost:27017/streamletDB", {
   useNewUrlParser: true,
 });
 
 const moviesSchema = new mongoose.Schema({
   adult: Boolean,
   backdrop_path: String,
-  genre_ids: [Number],
+  genre_ids: Array,
+  genres: Array,
   id: Number,
   original_language: String,
   original_title: String,
@@ -25,7 +27,28 @@ const moviesSchema = new mongoose.Schema({
   vote_count: Number,
 });
 
+const seriesSchema = new mongoose.Schema({
+  adult: Boolean,
+  backdrop_path: String,
+  id: Number,
+  name: String,
+  original_language: String,
+  original_name: String,
+  overview: String,
+  poster_path: String,
+  media_type: String,
+  genre_ids: Array,
+  genres: Array,
+  popularity: Number,
+  first_air_date: String,
+  title: String,
+  vote_average: Number,
+  vote_count: Number,
+  origin_country: Array,
+});
+
 const Movies = mongoose.model("Movies", moviesSchema);
+const Series = mongoose.model("Series", seriesSchema);
 
 const app = express();
 
@@ -33,6 +56,17 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Fetch movies from API
+const moviesUrl =
+  "https://api.themoviedb.org/3/discover/movie?include_adult=false&language=en-US&page=1";
+
+const seriesUrl =
+  "https://api.themoviedb.org/3/trending/tv/week?language=en-US";
+
+const newMoviesUrl =
+  "https://api.themoviedb.org/3/trending/movie/week?language=en-US";
+
+const popularMoviesUrl =
+  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
 
 const options = {
   method: "GET",
@@ -42,21 +76,70 @@ const options = {
   },
 };
 
-async function fetchMovies() {
+// Function to fetch movies from API
+async function fetchMovies(url) {
   try {
-    const response = await fetch(
-      "https://api.themoviedb.org/3/discover/movie?include_adult=false",
-      options
-    );
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      throw new Error(
+        "Movies cannot be loaded at the moment 😢, please try again"
+      );
+    }
+
     const data = await response.json();
-    console.log(data.results.length);
-  } catch (error) {}
+    return data.results;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
-fetchMovies();
+// fetchMovies(seriesUrl);
 
-app.get("/", function (req, res) {
-  res.send("Hello, Nodejs!");
+// Function to write movies data from API to database
+
+async function addMoviesToDatabase() {
+  const movieData = await fetchMovies(moviesUrl);
+
+  const updatedData = movieData.map((movie) => {
+    const genreArr = movie.genre_ids;
+    const movieGenres = [];
+    genres.forEach((genre) => {
+      for (let i = 0; i < genreArr.length; i++) {
+        const genreId = genreArr[i];
+
+        if (genre.id === genreId) {
+          movieGenres.push(genre.name);
+        }
+      }
+    });
+
+    return {
+      ...movie,
+      genres: movieGenres,
+    };
+  });
+
+  for (let i = 0; i <= updatedData.length; i++) {
+    const movie = updatedData[i];
+    // if(movie.id)
+    const dataMovie = new Movies(movie);
+
+    // await dataMovie.save();
+  }
+}
+
+// await addMoviesToDatabase();
+
+app.get("/movies", function (req, res) {
+  const movies = Movies.find()
+    .then((movie) => {
+      // console.log(movie);
+      res.send(movie);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.listen("5000", () => {
